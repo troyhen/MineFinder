19110fce252d73cbbc7e36c55cea1dfc508c42d9
troyhen <troyhen@gmail.com>
2019-03-29 08:42:24 -0600

    Add a draggable info panel

--- app/src/main/java/com/troy/mine/game/GameFragment.kt
+++ app/src/main/java/com/troy/mine/game/GameFragment.kt
@@ -2,17 +2,23 @@ package com.troy.mine.game
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.troy.mine.R
 import kotlinx.android.synthetic.main.fragment_game.*
 import org.koin.android.ext.android.inject
+import kotlin.math.roundToInt
+
 
 class GameFragment : Fragment() {
 
     private val gameEngine: GameEngine by inject()
+    private var windowX: Int = 0
+    private var windowY: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,7 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupListeners()
         setupObservers()
     }
 
@@ -33,6 +40,29 @@ class GameFragment : Fragment() {
         super.onStop()
         gameEngine.viewport = gameView.currentViewport
         gameEngine.save()
+    }
+
+    private fun setupListeners() {
+        dragHandle.setOnTouchListener { v, event ->
+            val x = event.rawX.roundToInt()
+            val y = event.rawY.roundToInt()
+            when (event.action and MotionEvent.ACTION_MASK) {
+                MotionEvent.ACTION_DOWN -> {
+                    val layoutParams = infoPanel.layoutParams as FrameLayout.LayoutParams
+                    windowX = x - layoutParams.leftMargin
+                    windowY = y - layoutParams.topMargin
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val layoutParams = infoPanel.layoutParams as FrameLayout.LayoutParams
+                    layoutParams.leftMargin = x - windowX
+                    layoutParams.topMargin = y - windowY
+                    infoPanel.layoutParams = layoutParams
+                }
+                else -> {
+                }
+            }
+            return@setOnTouchListener true
+        }
     }
 
     private fun setupObservers() {
