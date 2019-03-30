bf6d6da2f53fde6d6bb7cd2c1e87698abdb9c60c
troyhen <troyhen@gmail.com>
2019-03-29 21:29:21 -0600

    Improve dragging of infoPanel

--- app/src/main/java/com/troy/mine/game/GameFragment.kt
+++ app/src/main/java/com/troy/mine/game/GameFragment.kt
@@ -5,20 +5,18 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.troy.mine.R
 import kotlinx.android.synthetic.main.fragment_game.*
 import org.koin.android.ext.android.inject
-import kotlin.math.roundToInt
 
 
 class GameFragment : Fragment() {
 
     private val gameEngine: GameEngine by inject()
-    private var windowX: Int = 0
-    private var windowY: Int = 0
+    private var windowX: Float = 0f
+    private var windowY: Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,25 +41,27 @@ class GameFragment : Fragment() {
     }
 
     private fun setupListeners() {
-        dragHandle.setOnTouchListener { v, event ->
-            val x = event.rawX.roundToInt()
-            val y = event.rawY.roundToInt()
-            when (event.action and MotionEvent.ACTION_MASK) {
+        var dx = 0f
+        var dy = 0f
+        dragHandle.setOnTouchListener { _, event ->
+            return@setOnTouchListener when (event.action and MotionEvent.ACTION_MASK) {
                 MotionEvent.ACTION_DOWN -> {
-                    val layoutParams = infoPanel.layoutParams as FrameLayout.LayoutParams
-                    windowX = x - layoutParams.leftMargin
-                    windowY = y - layoutParams.topMargin
+                    dx = infoPanel.translationX - event.rawX
+                    dy = infoPanel.translationY - event.rawY
+                    true
                 }
                 MotionEvent.ACTION_MOVE -> {
-                    val layoutParams = infoPanel.layoutParams as FrameLayout.LayoutParams
-                    layoutParams.leftMargin = x - windowX
-                    layoutParams.topMargin = y - windowY
-                    infoPanel.layoutParams = layoutParams
+                    infoPanel.translationX = event.rawX + dx
+                    infoPanel.translationY = event.rawY + dy
+                    true
                 }
-                else -> {
+                MotionEvent.ACTION_UP -> {
+                    windowX = infoPanel.translationX
+                    windowY = infoPanel.translationY
+                    true
                 }
+                else -> false
             }
-            return@setOnTouchListener true
         }
     }
 
